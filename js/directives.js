8e1fd1095d8114d85bf0a5a95dee7afd39719b77
Rex Prosper C. Adrivan <adrivanrex@gmail.com>
2022-10-15 20:26:34 +0800

    close

--- js/directives.js
+++ js/directives.js
@@ -83,9 +83,9 @@ function pageTitle($rootScope, $timeout) {
         link: function(scope, element) {
             var listener = function(event, toState, toParams, fromState, fromParams) {
                 
-                var title = 'Kwarta';
+                var title = 'kwarta';
                 // Create your own title pattern
-                if (toState.data && toState.data.pageTitle) title = 'KWARTA | ' + toState.data.pageTitle;
+                if (toState.data && toState.data.pageTitle) title = ' ' + toState.data.pageTitle;
                 $timeout(function() {
                     element.text(title);
                 });
